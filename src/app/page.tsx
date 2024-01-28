ea2f4d3e086d81e72346da6f2e95c321756a317e
Chetan baliyan <chetanbaliyan10@gmail.com>
2024-01-28 23:52:38 +0530

    Update global styles and add Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Projects from "@/pages/Projects";
 import Image from "next/image";
 import Project from "./projects/page";
 import Achievement from "@/pages/Achievement";
+import Hero from "@/pages/Hero";
 
 export default function Home() {
   return (
-    <main className="flex bg-black text-white font-mono gap-20 min-h-screen flex-col    items-center   ">
+    <main className="flex bg-white  text-[#212121] font-mono gap-20 min-h-screen flex-col    items-center   ">
+      <Hero />
       <About />
       <Achievement />
       <Project />
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,20 +3,19 @@
 import React from "react";
 
 const About = () => {
-  const [showWhyNonTech, setShowWhyNonTech] = React.useState(false);
   return (
     <div className=" items-center justify-center w-full flex flex-col min-h-screen ">
-      <div className=" min-h-screen w-[100%] flex gap-8 justify-center flex-col items-center ">
+      <div className=" min-h-screen w-[100%] flex gap-3 justify-center flex-col items-center ">
         <h1 className="text-3xl">
-          Hey there! I&apos;m{" "}
-          <span className="text-blue-500">Chetan Baliyan</span>
+          Hey There! I&apos;m{" "}
+          <span className="text-blue-500">Chetan Baliyan.</span>
         </h1>
         <h3 className="text-2xl">I love building stuff</h3>
 
-        <div className="  gap-3 flex flex-col p-3 border w-[80%]">
-          <h1 className="text-base">About Me</h1>
+        <div className="  gap-3 flex flex-col items-center p-3  w-[80%]">
+          {/* <h1 className="text-base">Abo</h1> */}
           <div className="gap-10 flex flex-col">
-            <p className="text-base">
+            <p className="text-base text-center">
               My coding journey started in May 2022 when I decided to explore
               more about coding and building websites. I began by learning basic
               HTML, CSS, and JS. Addicted to coding, I dedicated 10-15 hours
@@ -31,28 +30,6 @@ const About = () => {
               like an AI Telegram chatbot and tools to extract structured info
               from PDFs and many others.
             </p>
-
-            <p
-              className="cursor-pointer m"
-              onClick={() => {
-                setShowWhyNonTech(!showWhyNonTech);
-              }}
-            >
-              =: Click here if you are curious about why a guy with a non-tech
-              degree is doing tech stuff.
-            </p>
-
-            {showWhyNonTech && (
-              <p>
-                From a young age, I was very interested in computers and their
-                languages. However, my greater passion was cricket, so I
-                didn&apos;t focus much on learning computers. I played cricket
-                at different levels, investing a lot of time in it. I chose an
-                easier, non-tech course in college, but, in the end, I had to
-                leave it due to some personal reasons. So, I thought, &apos;Now
-                what?&apos; That&apos;s when I decided to get into Tech.
-              </p>
-            )}
           </div>
         </div>
       </div>
